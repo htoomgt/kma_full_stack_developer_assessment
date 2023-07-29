ccdfa2758c4968a5cf4920d72ec7eaa4b3ee419f
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-07-29 10:22:06 +0630

    splash page login form frame started

---
+++ src/components/GreetingCard.jsx
@@ -0,0 +1,28 @@
+import { Grid, Typography, Box } from "@mui/material";
+import bgClipArt from "../assets/bg_clip_art.png";
+
+const GreetingCard = () => {
+    return (
+        <Grid item xs={6} className={`greetingCard`}>
+            <Typography variant="p" className={`orgTitle`}>
+                KMA Group of Companies Enterprise Portal
+            </Typography>
+            <Box className={`discriptionBlock`}>
+                <p className={`orgDescription`}>
+                    Kaung Myanmar Aung (KMA) is a group of companies registered
+                    under The Myanmar Companies Act. One of Myanmar’s renowned
+                    and respected organizations, the foundations of the group
+                    were laid almost 25 years ago.
+                </p>
+                <p className={`orgDescription`}>
+                    KMA Group has a broad and diversified portfolio of interests
+                    and our associated companies operate across almost all
+                    sectors of Myanmar’s economy.
+                </p>
+            </Box>
+            <img src={bgClipArt} className={`bgClipArt`} />
+        </Grid>
+    );
+};
+
+export default GreetingCard;
--- src/pages/SpalashPage.jsx
+++ src/pages/SpalashPage.jsx
@@ -1,34 +1,21 @@
 import { Grid, Typography, Box } from "@mui/material";
 import "./SpalashPage.css";
-import bgClipArt from "../assets/bg_clip_art.png";
+import GreetingCard from "../components/GreetingCard";
 
 const SpalashPage = () => {
     document.title = "KMA - Splash Page";
     return (
         <>
             <Grid container spacing={2} className={`gridMainConatiner`}>
-                <Grid item xs={6} className={`greetingCard`}>
-                    <Typography variant="p" className={`orgTitle`}>
-                        KMA Group of Companies Enterprise Portal
-                    </Typography>
-                    <Box className={`discriptionBlock`}>
-                        <p className={`orgDescription`}>
-                            Kaung Myanmar Aung (KMA) is a group of companies
-                            registered under The Myanmar Companies Act. One of
-                            Myanmar’s renowned and respected organizations, the
-                            foundations of the group were laid almost 25 years
-                            ago.
-                        </p>
-                        <p className={`orgDescription`}>
-                            KMA Group has a broad and diversified portfolio of
-                            interests and our associated companies operate
-                            across almost all sectors of Myanmar’s economy.
-                        </p>
+                <GreetingCard />
+                <Grid item xs={6} className={`loginGrid`}>
+                    <Box className={`loginDisplayBox`}>
+                        <Typography variant="p" className={`loginFormTitle`}>
+                            Welcome from the KMA Group of Companies! Please
+                            Login here
+                        </Typography>
+                        <Box className={`lognFormBox`}>Login box</Box>
                     </Box>
-                    <img src={bgClipArt} className={`bgClipArt`} />
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography variant="h1">Splash Page</Typography>
                 </Grid>
             </Grid>
         </>
