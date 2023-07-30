db1fb48eb5aa1ee95e1fc23ef02fc59515ce0c72
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-07-30 11:38:17 +0630

    splash screen redirect if logged in

--- src/pages/SpalashPage.jsx
+++ src/pages/SpalashPage.jsx
@@ -2,9 +2,21 @@ import { Grid } from "@mui/material";
 import "./SpalashPage.css";
 import GreetingCard from "../components/GreetingCard";
 import LoginCard from "../components/LoginCard";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
+import { loggedInUserState } from "../stores/user_recoil_store";
 
 const SpalashPage = () => {
     document.title = "KMA - Splash Page";
+    let navigate = useNavigate();
+    const loggedInUser = useRecoilValue(loggedInUserState);
+    useEffect(() => {
+        if (loggedInUser.status === true) {
+            navigate("/home");
+        }
+    }, [loggedInUser.status, navigate]);
+
     return (
         <>
             <Grid container spacing={2} className={`gridMainConatiner`}>
