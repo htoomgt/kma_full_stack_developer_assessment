f59fa6176f6dba2478643cca95e4d9d13b884624
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-08-05 01:00:18 +0630

    todo section complete

--- src/components/homePage/TodoGrid.jsx
+++ src/components/homePage/TodoGrid.jsx
@@ -1,5 +1,6 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography, Button, Box } from "@mui/material";
 import PropTypes from "prop-types";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
 const TodoGrid = ({ id }) => {
     return (
         <>
@@ -7,18 +8,139 @@ const TodoGrid = ({ id }) => {
                 <Grid
                     item
                     xs={12}
-                    sm={6}
-                    sx={{ height: "320px", backgroundColor: "teal" }}
+                    sm={12}
+                    sx={{
+                        height: "200px",
+                        backgroundColor: "none",
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                        flexDirection: "row",
+                        padding: "32px",
+                    }}
                 >
-                    to do left panel
+                    <WbSunnyOutlinedIcon
+                        sx={{ fontSize: "36px", marginRight: "8px" }}
+                    />
+                    <Typography variant="h5"> My Day (To Do)</Typography>
                 </Grid>
                 <Grid
                     item
                     xs={12}
                     sm={6}
-                    sx={{ height: "320px", backgroundColor: "lightBlue" }}
+                    sx={{ minHeight: "320px", backgroundColor: "none" }}
                 >
-                    to do right panel
+                    <Box
+                        sx={{
+                            marginTop: "16px",
+                            padding: "32px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "flex-start",
+                            flexDirection: "column",
+                            fontFamily: "Montserrat sans-serif",
+                        }}
+                    >
+                        <Typography variant="h6">Task #1</Typography>
+                        <Typography
+                            variant="p"
+                            sx={{ fontSize: "14px", marginTop: "16px" }}
+                        >
+                            Nemo enim ipsam voluptatem quia voluptas sit
+                            aspernatur aut odit aut fugit, sed quia consequuntur
+                            magni dolores eos qui ratione voluptatem sequi
+                            nesciunt. Neque porro quisquam est.
+                        </Typography>
+
+                        <Button variant="outlined" sx={{ marginTop: "16px" }}>
+                            View More
+                        </Button>
+                    </Box>
+                    <Box
+                        sx={{
+                            marginTop: "16px",
+                            padding: "32px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "flex-start",
+                            flexDirection: "column",
+                            fontFamily: "Montserrat sans-serif",
+                        }}
+                    >
+                        <Typography variant="h6">Task #2</Typography>
+                        <Typography
+                            variant="p"
+                            sx={{ fontSize: "14px", marginTop: "16px" }}
+                        >
+                            Consequuntur magni dolores eos qui ratione
+                            voluptatem sequi nesciunt. Neque porro quisquam est,
+                            qui dolorem ipsum quia dolor sit amet, consectetur,
+                            adipisci velit, sed quia non numquam.
+                        </Typography>
+
+                        <Button variant="outlined" sx={{ marginTop: "16px" }}>
+                            View More
+                        </Button>
+                    </Box>
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    sx={{ minHeight: "320px", backgroundColor: "none" }}
+                >
+                    <Box
+                        sx={{
+                            marginTop: "16px",
+                            padding: "32px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "flex-start",
+                            flexDirection: "column",
+                            fontFamily: "Montserrat sans-serif",
+                        }}
+                    >
+                        <Typography variant="h6">Task #3</Typography>
+                        <Typography
+                            variant="p"
+                            sx={{ fontSize: "14px", marginTop: "16px" }}
+                        >
+                            Amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation
+                            ullamco laboris nisi.
+                        </Typography>
+
+                        <Button variant="outlined" sx={{ marginTop: "16px" }}>
+                            View More
+                        </Button>
+                    </Box>
+                    <Box
+                        sx={{
+                            marginTop: "16px",
+                            padding: "32px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "flex-start",
+                            flexDirection: "column",
+                            fontFamily: "Montserrat sans-serif",
+                        }}
+                    >
+                        <Typography variant="h6">Task #4</Typography>
+                        <Typography
+                            variant="p"
+                            sx={{ fontSize: "14px", marginTop: "16px" }}
+                        >
+                            Eos qui ratione voluptatem sequi nesciunt. Neque
+                            porro quisquam est, qui dolorem ipsum quia dolor sit
+                            amet, consectetur, adipisci velit, sed quia non
+                            numquam eius modi tempora.
+                        </Typography>
+
+                        <Button variant="outlined" sx={{ marginTop: "16px" }}>
+                            View More
+                        </Button>
+                    </Box>
                 </Grid>
             </Grid>
         </>
